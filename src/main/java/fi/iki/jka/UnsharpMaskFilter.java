c82f21f306ca51d8a46aecb7c77174e8fdf97400
Jari Karjala <jka@iki.fi>
2020-01-20 18:44:08 +0200

    Updated web page

--- src/main/java/fi/iki/jka/UnsharpMaskFilter.java
+++ src/main/java/fi/iki/jka/UnsharpMaskFilter.java
@@ -1,7 +1,7 @@
 
 /*
 UnsharpMaskFilter for JAlbum
-Copyright (C) 2003  Janne Kipinä
+Copyright (C) 2003  Janne Kipina
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
